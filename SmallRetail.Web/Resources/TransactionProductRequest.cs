a94523fd41c91d2c7a8356368c7e1efb3dfde6b6
mfaizudd <mfaizudd@gmail.com>
2021-06-16 14:11:05 +0700

    fix(api): remove transaction id from request
    because its generated

--- SmallRetail.Web/Resources/TransactionProductRequest.cs
+++ SmallRetail.Web/Resources/TransactionProductRequest.cs
@@ -7,7 +7,6 @@ namespace SmallRetail.Web.Resources
 {
     public class TransactionProductRequest
     {
-        public Guid TransactionId { get; set; }
         public Guid ProductId { get; set; }
         public int Quantity { get; set; }
     }
