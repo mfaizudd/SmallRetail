e2a13cbc094c68a5bf8e9608d2fd3c55aaa2ce64
mfaizudd <mfaizudd@gmail.com>
2021-07-08 21:40:44 +0700

    fix(api): remove transaciton model
    remove it from transaction product response because the transaction
    information is not needed there

--- SmallRetail.Web/Resources/TransactionProductResponse.cs
+++ SmallRetail.Web/Resources/TransactionProductResponse.cs
@@ -5,7 +5,6 @@ namespace SmallRetail.Web.Resources
     public class TransactionProductResponse
     {
         public Guid TransactionId { get; set; }
-        public TransactionResponse Transaction {get; set; }
         public Guid ProductId { get; set; }
         public ProductResponse Product { get; set; }
         public int Quantity { get; set; }
