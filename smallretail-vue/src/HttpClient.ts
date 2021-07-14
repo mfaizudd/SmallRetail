5004ecd109fa78dbd222878df8094b278c2b9fa0
mfaizudd <mfaizudd@gmail.com>
2021-07-14 20:17:26 +0700

    fix: use default httpclient

--- smallretail-vue/src/HttpClient.ts
+++ smallretail-vue/src/HttpClient.ts
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios from 'axios'
 
 export default axios.create({
   baseURL: process.env.VUE_APP_API_URL,
   headers: {
-    "Content-Type": "application/json"
+    'Content-Type': 'application/json'
   }
-});
+})
--- smallretail-vue/src/services/ProductService.ts
+++ smallretail-vue/src/services/ProductService.ts
@@ -1,37 +1,28 @@
-import Product from '@/types/Product';
-import axios from 'axios';
-
-export default class ProductService
-{
-  API_URL = process.env.VUE_APP_API_URL
-
-  public async getProducts(limit: number = 10, page: number = 1): Promise<Product>
-  {
-    let result = await axios.get(`${this.API_URL}/products?limit=${limit}&page=${page}`);
-    return result.data;
+import Product from '@/types/Product'
+import HttpClient from '@/HttpClient'
+export default class ProductService {
+  public async getProducts (limit = 10, page = 1): Promise<Product> {
+    const result = await HttpClient.get(`/products?limit=${limit}&page=${page}`)
+    return result.data
   }
 
-  public async getProduct(id: string): Promise<Product>
-  {
-    let result = await axios.get(`${this.API_URL}/products/${id}`);
-    return result.data;
+  public async getProduct (id: string): Promise<Product> {
+    const result = await HttpClient.get(`/products/${id}`)
+    return result.data
   }
 
-  public async create(product: Product): Promise<any>
-  {
-    let result = await axios.post(`${this.API_URL}/products`, product);
-    return result.data;
+  public async create (product: Product): Promise<any> {
+    const result = await HttpClient.post('/products', product)
+    return result.data
   }
 
-  public async update(id: string, product: Product): Promise<any>
-  {
-    let result = await axios.put(`${this.API_URL}/products/${id}`, product);
-    return result.data;
+  public async update (id: string, product: Product): Promise<any> {
+    const result = await HttpClient.put(`/products/${id}`, product)
+    return result.data
   }
 
-  public async delete(id: string): Promise<any>
-  {
-    let result = await axios.delete(`${this.API_URL}/products/${id}`);
-    return result.data;
+  public async delete (id: string): Promise<any> {
+    const result = await HttpClient.delete(`/products/${id}`)
+    return result.data
   }
 }
