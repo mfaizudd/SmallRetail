d79c10c0b153bbeb661f98cdef313ce2531c5898
mfaizudd <mfaizudd@gmail.com>
2021-07-30 21:48:47 +0700

    fix(service): add get user

--- smallretail-vue/src/services/UserService.ts
+++ smallretail-vue/src/services/UserService.ts
@@ -7,4 +7,14 @@ export default class UserService {
     var result = await HttpClient.post('/login', request);
     return result.data;
   }
+
+  public async getUsers(limit = 10, page = 1): Promise<any> {
+    var result = await HttpClient.get(`/users?limit=${limit}&page=${page}`)
+    return result.data
+  }
+
+  public async getUser(id: string): Promise<User> {
+    var result = await HttpClient.get(`/users?id=${id}`);
+    return result.data
+  }
 }
