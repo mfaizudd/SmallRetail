53ddcb5e9a11908fa76b0edee0ad1b3e5437f2de
Muhammad Faizud Daroin <mfaizudd@gmail.com>
2022-07-31 17:52:50 +0700

    feat(ui): login page

--- smallretail-next/pages/_app.tsx
+++ smallretail-next/pages/_app.tsx
@@ -1,5 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import "primereact/resources/themes/lara-light-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
