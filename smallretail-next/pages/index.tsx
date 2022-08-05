992ac3f8304d5769bd5c694c7971d29b05a7a809
Muhammad Faizud Daroin <mfaizudd@gmail.com>
2022-08-05 21:01:46 +0700

    fix: update login page

--- smallretail-next/pages/index.tsx
+++ smallretail-next/pages/index.tsx
@@ -1,33 +1,50 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { Card } from 'primereact/card';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
+import { Checkbox } from 'primereact/checkbox';
 import { useState } from 'react';
+// import Image from 'next/image';
 
 const Home: NextPage = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [checked, setChecked] = useState(false);
   return (
-    <div className="flex p-5 justify-content-center">
+    <div className="surface-200">
       <Head>
         <title>SmallRetail App</title>
         <meta name="description" content="SmallRetail" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Card title="Login" className="min-w-min w-25rem">
-        <div className="flex flex-column gap-3">
-          <span className="p-float-label">
-            <InputText id="in" value={username} onChange={(e) => setUsername(e.target.value)} className="w-full" />
-            <label htmlFor="in">Username</label>
-          </span>
-          <span className="p-float-label">
-            <InputText id="in" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full" type="password" />
-            <label htmlFor="in">Password</label>
-          </span>
-          <Button label="Login" />
+      <div className="flex align-items-center justify-content-center min-h-screen">
+
+        <div className="surface-card p-4 shadow-2 border-round w-full lg:w-6">
+          <div className="text-center mb-5">
+            {/* <Image src="assets/images/blocks/logos/hyper.svg" alt="hyper" height={50} className="mb-3" /> */}
+            <div className="text-900 text-3xl font-medium mb-3">Welcome to SmallRetail</div>
+            <span className="text-600 font-medium line-height-3">{ "Don't have an account? Contact administrator" }</span>
+          </div>
+
+          <div>
+            <label htmlFor="username" className="block text-900 font-medium mb-2">Username</label>
+            <InputText id="username" type="text" className="w-full mb-3" value={username} onChange={e => setUsername(e.target.value)} />
+
+            <label htmlFor="password" className="block text-900 font-medium mb-2">Password</label>
+            <InputText id="password" type="password" className="w-full mb-3" value={password} onChange={e => setPassword(e.target.value)} />
+
+            <div className="flex align-items-center justify-content-between mb-6">
+              <div className="flex align-items-center">
+                <Checkbox id="rememberme" onChange={e => setChecked(e.checked)} checked={checked} className="mr-2" />
+                <label htmlFor="rememberme">Remember me</label>
+              </div>
+              <a className="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">Forgot your password?</a>
+            </div>
+
+            <Button label="Sign In" icon="pi pi-user" className="w-full" />
+          </div>
         </div>
-      </Card>
+      </div>
     </div>
   )
 }
